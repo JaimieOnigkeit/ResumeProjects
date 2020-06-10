a201ed6821e1fe9d28738fa6dd56dd8c05a66f7b
jaimieonigkeit <jaimie.onigkeit@gmail.com>
2020-06-10 15:43:34 -0600

    User and Posts database integration

--- flask_blog/flask_blog.py
+++ flask_blog/flask_blog.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cb7638a68ea3c63f7460e822fe2edb93'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+#Database for users
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+#Database for blog posts
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+    
+    
 posts = [
     {
         'author' : 'Jaimie Onigkeit',
@@ -19,15 +46,19 @@ posts = [
     }
 ]
 
+#Routes
+#Home page route
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+#About page route
 @app.route("/about")
 def about():
     return render_template('about.html', title = 'About')
 
+#Registration page route
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -36,6 +67,7 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
+#Login page route
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -47,5 +79,6 @@ def login():
             flash('Login failed', 'danger')
     return render_template('login.html', title='Login', form=form)
 
+#Runs app in debug mode when instatiated using python flask_blog.py
 if __name__ == '__main__':
     app.run(debug=True)
